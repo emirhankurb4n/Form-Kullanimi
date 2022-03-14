e50da70b9ebfb6621532886574c9d1b7b34208bf
Emirhan Kurban <100554100+emirhankurb4n@users.noreply.github.com>
2022-03-14 10:17:45 +0300

    kodlar

---
+++ Form2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication28
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            MessageBox.Show("FormB kapandı.");
+        }
+
+        private void Form2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("FormB click olayı çalışıyor.");
+        }
+    }
+}
